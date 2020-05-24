cd3769c225781eab977e1df0856139494535b32e
saichandrapandraju <saichandrapandraju@gmail.com>
2020-05-24 20:42:32 +0530

    code cleanup and adjusted nav

--- site.js
+++ site.js
@@ -28,7 +28,6 @@
 const hero = document.querySelector(".hero");
 const slider = document.querySelector(".slider");
 const logo = document.querySelector(".logo");
-const hamburger = document.querySelector(".hamburger");
 const headline = document.querySelector(".headline");
 const navLinks = document.querySelectorAll(".nav-links li");
 const nav = document.querySelector(".nav-links");
